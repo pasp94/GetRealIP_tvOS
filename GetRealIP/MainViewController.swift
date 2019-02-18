afdd1ae8349b4038e23ceb91c3d9c9f512b6394a
Pasquale <spisto.pasquale1994@gmail.com>
2019-02-19 00:48:44 +0100

    Created new MainViewController

---
+++ GetRealIP/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  GetRealIP
+//
+//  Created by Pasquale Spisto on 19/02/2019.
+//  Copyright © 2019 pasp_. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
